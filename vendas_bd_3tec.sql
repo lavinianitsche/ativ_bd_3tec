6f3f9d908c4c2d5f6e541e82f746aec8e15364eb
lavinianitsche <lavinia.nitsche@escola.pr.gov.br>
2025-09-01 10:58:10 -0300

    terceiro commit

--- vendas_bd_3tec.sql
+++ vendas_bd_3tec.sql
@@ -43,3 +43,17 @@ create table venda(
     estado varchar(255),
     id int(11)
 );
+
+create table estoque ( 
+	valor_unitario int (11),
+    quantidade varchar (100) not null,
+    id int primary key auto_increment,
+    remedio_id int (11)
+    );
+
+create table remedio (
+venda varchar(255),
+estoque varchar (255),
+id int primary key auto_increment,
+consumivel_id int (11)
+);
