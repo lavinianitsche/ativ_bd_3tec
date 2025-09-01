f921eee5ee184400cf96ad1474340fd77959f410
lavinianitsche <lavinia.nitsche@escola.pr.gov.br>
2025-09-01 10:53:58 -0300

    segundo commit

--- vendas_bd_3tec.sql
+++ vendas_bd_3tec.sql
@@ -17,3 +17,29 @@ create table perfil(
 	fotoUrl varchar(255),
 	id int(11)
 );
+
+create table login(
+	user varchar(255),
+	passaword varchar(255),
+	id int(11),
+	login_id int(11),
+	responsavel_id int(11)
+);
+
+create table agenda(
+	data date,
+	horainicio varchar(255),
+    horafim varchar(255),
+    disponivel varchar(255),
+    id int(11)
+);
+
+create table venda(
+	loja varchar(255),
+    valor_total varchar(255),
+    valor_item varchar(255),
+    data_compra date,
+    forma_pagamento varchar(255),
+    estado varchar(255),
+    id int(11)
+);
