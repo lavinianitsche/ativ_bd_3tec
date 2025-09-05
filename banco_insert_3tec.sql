5edcfa10698acc6c1d3f88c39cf664fc61ccd4d8
lavinianitsche <lavinia.nitsche@escola.pr.gov.br>
2025-09-05 09:13:20 -0300

    decimo commit

---
+++ banco_insert_3tec.sql
@@ -0,0 +1,29 @@
+use vendas;
+
+insert into agenda(data, horainicio, horafim, disponivel, id) values("2025-09-04", "12:00:00", "12:08:00", "sim", 4);
+
+insert into catalogo(item, descricao, id) values("cereja", "fruta", 5);
+
+insert into consumivel(venda, estoque, id, estoque_id) values("sla", "naosei", 7, 9);
+
+insert into endereco(rua, bairro, numero, cep, complemento, id, loja_id) values("av brasil", "guaruja", "4235", "12345-678", "apartamento", 8, 5);
+
+insert into estetica(venda, agenda, id, agenda_id) values("racao", "banho", 5, 2);
+
+insert into estoque(valor_unitario, quantidade, id, remedio_id) values(15, "cinco", 2, 8);
+
+insert into login(user, passaword, id, login_id, responsavel_id) values("lavinianitsche", 123, 8, 4, 2);
+
+insert into loja(nome, email, telefone, cnpj, endereco, id, pet_id, catalogo_id) values("lavinia", "lavi123@gmail", 123456789, "1234", "rua das palmeiras", 2, 4, 6);
+
+insert into perfil(fotoUrl, id) values("foto.jpg", 4);
+
+insert into pet(nome, idade, raca, porte, sexo, responsavel, id, perfil_id) values("amora", 6, "spitz", "pequeno", "feminino", "lavinia", 4, 8);
+
+select * from agenda;
+
+select * from catalogo;
+
+select * from consumivel;
+
+select * from endereco;
